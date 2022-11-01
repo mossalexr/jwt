0eb0e50c1806ba25d5666b8259039440ca330ab2
AlexMoss <moss.alexr@gmail.com>
2022-11-01 17:40:31 -0400

    push

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,16 @@
   const User = require('../models/User');
+  const jwt = require('jsonwebtoken');
 
   const handleErrors = (err) => {
     console.log(err.message, err.code)
     let errors = {email: '', password: ''};
+
+    if (err.message === 'incorrect email'){
+      errors.email = 'That email is not registered';
+    }
+    if (err.message === 'incorrect password'){
+      errors.password = 'That password is incorrect';
+    }
 
     //duplicate email error
     if(err.code == 11000){
@@ -10,12 +18,21 @@
       return errors;
     }
 
+
     if(err.message.includes('user validation failed')){
       Object.values(err.errors).forEach(({properties}) => {
         errors[properties.path] = properties.message
       })
     }
+    return errors;
   }
+
+  const maxAge = 3 * 24 * 60 * 60;
+    const createToken = (id) => {
+      return jwt.sign({id}, 'secret message', {
+        expiresIn: maxAge
+      })
+    }
 // controller actions
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -29,7 +46,9 @@ module.exports.signup_get = (req, res) => {
     const {email, password} = req.body;
     try {
       const user = await User.create({email, password})
-      res.status(201).json(user)
+      const token = createToken(user._id);
+      res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+      res.status(201).json({user: user._id})
     } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json({ errors })
@@ -39,5 +58,14 @@ module.exports.signup_get = (req, res) => {
   
   module.exports.login_post = async (req, res) => {
     const {email, password} = req.body;
-    console.log(email, password);
+
+    try {
+      const user = await User.login(email, password);
+      const token = createToken(user._id);
+      res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+      res.status(200).json({ user: user._id });
+    } catch (err) {
+      const errors = handleErrors(err);
+      res.status(400).json({ errors });
+    }
   }
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -22,10 +24,23 @@ userSchema.post('save', function(doc, next) {
 })
 
 //fire before doc saved to database
-userSchema.pre('save', function(next) {
-    console.log('user about to be created and saved', this)
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
     next();
 })
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+}
 
 const User = mongoose.model('user', userSchema);
 
