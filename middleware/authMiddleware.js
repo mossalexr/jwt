2cbff8f3d3e8e29caade0ad5db9cd538cd6e7de8
AlexMoss <moss.alexr@gmail.com>
2022-11-02 16:35:03 -0400

    push

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt
 
+
+    //check if json web token exists and is verified
     if(token){
         jwt.verify(token, 'secret message', (err, decodedToken) => {
             if (err) {
